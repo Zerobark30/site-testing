32dfd0f16f6110ce3eaac1a265e28fcb4b8e1791
Matthew Wells <matthewpwells@gmail.com>
2015-01-25 21:12:29 -0800

    Add css, js, images, tab-content folders and resources-used text file

---
+++ js/workspace.js
@@ -0,0 +1,7 @@
+var Name = "Audacity";
+var SubString1 = Name.substring(1,2);
+var SubString2 = Name.substring(2,8);
+var CapitalSubString1 = SubString1.toUpperCase();
+var Udacity = CapitalSubString1 + SubString2;
+
+console.log(Udacity);
